2c979e2b0ec30e50eef8fba68e46f4539b70d455
Lily Rlc <hibiinheaven@hotmail.com>
2014-12-02 18:37:15 +0100

    my first commit

---
+++ app/views/hello.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html>
+<head>
+	<title>CodePin</title>
+	<meta charset="utf-8">
+	<meta name ="description" content="Google voit cela">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Grand+Hotel" />
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+
+	<div class="heading">
+
+		<nav class="navbar navbar-default" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Coucou</a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="#">Link</a></li>
+		        <li><a href="#">Link</a></li>
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+		          <ul class="dropdown-menu" role="menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		            <li><a href="#">Something else here</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#">Separated link</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#">One more separated link</a></li>
+		          </ul>
+		        </li>
+		      </ul>
+		      <form class="navbar-form navbar-left" role="search">
+		        <div class="form-group">
+		          <input type="text" class="form-control" placeholder="Search">
+		        </div>
+		        <button type="submit" class="btn btn-default">Submit</button>
+		      </form>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Link</a></li>
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+		          <ul class="dropdown-menu" role="menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		            <li><a href="#">Something else here</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#">Separated link</a></li>
+		          </ul>
+		        </li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+
+		<h1> Scuba your dream  </h1>
+		<a href="#salutlesterriens" class="btn btn-default">Découvrir</a>
+	</div>
+	
+	<div class="row colonnes">
+		<div class="col-xs-4 col-md-4">
+			<b>Rêver</b>
+			<br> <i class="fa fa-bug fa-5x"></i> </br>
+			<p> Victor Hugo was born on February 26, 1802, in Besançon, France. After training as a lawyer, Hugo embarked on the literary career. He became one of the most important French Romantic poets, novelists and dramatists of his time, having assembled a massive body of work while living in Paris, Brussels and the Channel Islands. Hugo died on May 22, 1885, in Paris.
+			Hugo fled to Brussels following a coup in 1851. He lived in Brussels and in Britain until his return to France in 1870. Much of the work that Hugo published during this period conveys biting sarcasm and fierce social criticism. Among these works is the novel Les Misérables, was finally published in 1862. The book was an immediate success in Europe and the United States. Later reinterpreted as a theatrical musical and a film, Les Misérables remains one of the best-known works of 19th century literature. </p>
+		</div>
+		<div class="col-xs-4 col-md-4">
+			<b>Fantasmer</b>
+			<br> <i class="fa fa-globe fa-5x"></i> </br>
+			<p>Victor-Marie Hugo was born in Besançon, France, on February 26, 1802, to mother Sophie Trébuche and father Joseph-Léopold-Sigisbert Hugo. His father was a military officer who later served as a general under Napoleon.
+			Though Hugo returned to France after 1870 as a symbol of republican triumph, his later years were largely sad. He lost two sons between 1871 and 1873. His later works are somewhat darker than his earlier writing, focusing on themes of God, Satan and death. </p>
+		</div>
+		<div class="col-xs-4 col-md-4">
+			<b>Planner</b>
+			<br> <i class="fa fa-paper-plane-o fa-5x"></i> </br>
+			<p> In 1831, he published one of his most enduring works, Notre-Dame de Paris (The Hunchback of Notre Dame). Set in the medieval period, the novel presents a harsh criticism of the society that degrades and shuns the hunchback Quasimodo. This was Hugo's most celebrated work to date, and paved the way for his subsequent political writing.
+			In 1878, he was stricken with cerebral congestion. Hugo and his mistress, Juliette, continued to live in Paris for the rest of their lives. The street on which he lived was renamed Avenue Victor Hugo on the occasion of his 80th birthday in 1882. Juliette died the following year and Victor Hugo died in Paris on May 22, 1885. He received a hero's funeral. His body lay in state beneath the Arc de Triomphe before burial in the Panthéon.</p>
+		</div>
+	</div>
+<section id="salutlesterriens"> 
+	<br> In 1878, he was stricken with cerebral congestion. Hugo and his mistress, Juliette, continued to live in Paris for the rest of their lives. The street on which he lived was renamed Avenue Victor Hugo on the occasion of his 80th birthday in 1882. Juliette died the following year and Victor Hugo died in Paris on May 22, 1885. He received a hero's funeral. His body lay in state beneath the Arc de Triomphe before burial in the Panthéon.In 1831, he published one of his most enduring works, Notre-Dame de Paris (The Hunchback of Notre Dame). Set in the medieval period, the novel presents a harsh criticism of the society that degrades and shuns the hunchback Quasimodo. This was Hugo's most celebrated work to date, and paved the way for his subsequent political writing.
+
+A prolific writer, Hugo was established as one of the most celebrated literary figures in </br> </section>
+		
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
